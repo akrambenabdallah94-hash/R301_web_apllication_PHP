cd9588098ee595efb85468474393a8c5bc2edd6c
ak94u <akrambenabdallah94@gmail.com>
2026-09-18 23:26:43 +0200

    TD1 commencé exo début exo 4

---
+++ TD1/echo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title> Insérer le titrer ici </title>
+    </head>
+    <body>
+        <?php
+            $marque = "Renault";
+            $couleur = "bleu"
+            $immatriculation = "BW-315-CZ";
+            $nbSieges = 5;
+        ?>
+        
+
+    </body>
+</html>
+
+
+
