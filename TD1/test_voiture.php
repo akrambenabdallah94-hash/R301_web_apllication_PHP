40fde135d226a99e27b4f5d484c4077bbe219b8f
ak94u <akrambenabdallah94@gmail.com>
2026-09-20 23:18:04 +0200

    Td6 terminé

---
+++ TD1/test_voiture.php
@@ -0,0 +1,19 @@
+<DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title> Insérer le titrer ici </title>
+    </head>
+    <body>
+        <?php
+            require_once "C:\wamp64\www\R301_web\TD1\Voiture.php";
+
+            $BMW = new Voiture('AB-123-CD', 'BMW', 'noir', 5);
+            $Renault = new Voiture('EF-458-GH', 'Ranault', 'blanc', 5);
+
+            echo $BMW;
+            echo "<br>$Renault</br>";
+
+        ?>
+    </body> 
+</html> 
