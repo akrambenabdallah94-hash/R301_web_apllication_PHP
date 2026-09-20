48981df43f7700705befc28271c6fe0c0d572021
ak94u <akrambenabdallah94@gmail.com>
2026-09-20 03:46:20 +0200

    Partis 2 de l'exo 4 terminé.

--- TD1/echo.php
+++ TD1/echo.php
@@ -6,16 +6,37 @@
     </head>
     <body>
         <?php
-            $voiture = array(
+            $BMW = array(
+                'immatriculation' => 'AB-123-CD',
                 'marque' => 'BMW',
                 'couleur' => 'noir',
-                'immatriculation' => 'BW-318-AB',
                 'nbSieges' => 5
             );
 
-            echo "<p> Voiture {$voiture['immatriculation']} de marque {$voiture['marque']} (couleur {$voiture['couleur']}, {$voiture['nbSieges']} sièges) </p>";
+            $Renault = array(
+                'immatriculation' => 'EF-456-GH',
+                'marque' => 'Renault',
+                'couleur' => 'blanc',
+                'nbSieges' => 4
+            );
+
+            $voiture = array(
+            );
+
+            var_dump($voiture);
+
+            if(empty($voiture)){
+                echo "Le tableau est vide";
+            } else {
+                echo "Le tableau n'est pas vide";
+            }
+
+            echo "<ul>
+                <li>Voiture 1 : {$voiture['1']['immatriculation']} de marque {$voiture['1']['marque']} (couleur {$voiture['1']['couleur']}, {$voiture['1']['nbSieges']} sièges) </li>
+                <li>Voiture 2 : {$voiture['2']['immatriculation']} de marque {$voiture['2']['marque']} (couleur {$voiture['2']['couleur']}, {$voiture['2']['nbSieges']} sièges) </li>
+                </ul>";
         ?>
-    </body>
+    </body> 
 </html>
 
 
