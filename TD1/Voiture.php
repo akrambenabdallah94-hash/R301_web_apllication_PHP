ccbf202a38b6e39b188533aa53b231aaae4b5b17
ak94u <akrambenabdallah94@gmail.com>
2026-09-23 13:38:37 +0200

    On type la class Voiture.php

--- TD1/Voiture.php
+++ TD1/Voiture.php
@@ -1,53 +1,53 @@
 <?php
-    class Voiture {
-        private $immatriculation;
-        private $marque;
-        private $couleur;
-        private $nbSieges;
+        class Voiture {
+            private string $immatriculation;
+            private string $marque;
+            private string $couleur;
+            private int $nbSieges;
 
-        //un getter
-        public function getMarque(){
-            return $this->marque;
-        }
+            //un getter
+            public function getMarque(): string{
+                return $this->marque;
+            }
 
-        public function setMarque($marque){
-            $this->marque = $marque;
-        }
+            public function setMarque(string $marque): void{
+                $this->marque = $marque;
+            }
 
-        public function getImmatriculation(){
-            return $this->immatriculation;
-        }
+            public function getImmatriculation(): string{
+                return $this->immatriculation;
+            }
 
-        public function setImmatriculation($immatriculation){
-            $this->immatriculation = substr($immatriculation, 0, 8); // On ne stock que les 8 premiers caractères de l'immatriculation
-        }
+            public function setImmatriculation(string $immatriculation): void{
+                $this->immatriculation = substr($immatriculation, 0, 8); // On ne stock que les 8 premiers caractères de l'immatriculation
+            }
 
-        public function getCouleur(){
-            return $this->couleur;
-        }
+            public function getCouleur(): string{
+                return $this->couleur;
+            }
 
-        public function setCouleur($couleur){
-            $this->couleur = $couleur;
-        }
+            public function setCouleur(string $couleur): void{
+                $this->couleur = $couleur;
+            }
 
-        public function getNbSieges(){
-            return $this->nbSieges;
-        }
+            public function getNbSieges(): int{
+                return $this->nbSieges;
+            }
 
-        public function setNbSieges($nbSieges){
-            $this->nbSieges = $nbSieges;
-        }
+            public function setNbSieges(int $nbSieges): void{
+                $this->nbSieges = $nbSieges;
+            }
 
 
-        public function __construct($immatriculation, $marque, $couleur, $nbSieges){
-            $this->immatriculation = substr($immatriculation,0, 8);
-            $this->marque = $marque;
-            $this->couleur = $couleur;
-            $this->nbSieges = $nbSieges;
-        }
+            public function __construct(string $immatriculation, string $marque, string $couleur, int $nbSieges){
+                $this->immatriculation = substr($immatriculation,0, 8);
+                $this->marque = $marque;
+                $this->couleur = $couleur;
+                $this->nbSieges = $nbSieges;
+            }
 
-        public function __toString(){
-            return "Voiture {$this->immatriculation} de marque {$this->marque} (couleur {$this->couleur}, {$this->nbSieges} sièges)";
-        }
-    }   
+            public function __toString(): string{
+                return "Voiture {$this->immatriculation} de marque {$this->marque} (couleur {$this->couleur}, {$this->nbSieges} sièges)";
+            }
+        }   
 ?>
